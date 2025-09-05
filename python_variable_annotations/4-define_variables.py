bd5e47f26d36e8fe8b35db09d990fc9fdd37c4aa
missmbuvi04 <156191743+missmbuvi04@users.noreply.github.com>
2025-09-05 10:47:25 +0300

    Update 4-define_variables.py

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,6 +1,7 @@
-"""Define variables
-    Define and annotate the following variables with the specified values """
+#!/usr/bin/env python3
+"""Contains definition of variables with specified values and annotations."""
+
 a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
-school: str = 'Holberton'
+school: str = "Holberton"
